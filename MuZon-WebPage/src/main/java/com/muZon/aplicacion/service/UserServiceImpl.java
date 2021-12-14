fb6fee93b5006659976cee21c307cb3d91c92771
ander1p <ander.palacios@alumni.mondragon.edu>
2021-12-14 10:15:06 +0100

    webpage CRUD, login and register via administrator

---
+++ MuZon-WebPage/src/main/java/com/muZon/aplicacion/service/UserServiceImpl.java
@@ -0,0 +1,126 @@
+package com.muZon.aplicacion.service;
+
+import java.util.Optional;
+
+
+import com.muZon.aplicacion.dto.ChangePasswordForm;
+import com.muZon.aplicacion.entity.User;
+import com.muZon.aplicacion.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+	@Autowired
+	UserRepository repository;
+
+	@Override
+	public Iterable<User> getAllUsers() {
+		return repository.findAll();
+	}
+	
+	@Autowired
+	BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	private boolean checkUsernameAvailable(User user) throws Exception {
+		Optional<User> userFound = repository.findByUsername(user.getUsername());
+		if (userFound.isPresent()) {
+			throw new Exception("Username no disponible");
+		}
+		return true;
+	}
+
+	private boolean checkPasswordValid(User user) throws Exception {
+		if (user.getConfirmPassword() == null || user.getConfirmPassword().isEmpty()) {
+			throw new Exception("Confirm Password es obligatorio");
+		}
+		
+		if ( !user.getPassword().equals(user.getConfirmPassword())) {
+			throw new Exception("Password y Confirm Password no son iguales");
+		}
+		return true;
+	}
+
+
+	@Override
+	public User createUser(User user) throws Exception {
+		if (checkUsernameAvailable(user) && checkPasswordValid(user)) {
+	
+			user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+			user = repository.save(user);
+		}
+		return user;
+	}
+
+	@Override
+	public User getUserById(Long id) throws Exception {
+		return repository.findById(id).orElseThrow(() -> new Exception("El usuario no existe."));
+	}
+
+	@Override
+	public User updateUser(User fromUser) throws Exception {
+		User toUser = getUserById(fromUser.getId());
+		mapUser(fromUser, toUser);
+		return repository.save(toUser);
+	}
+	
+	/**
+	 * Map everythin but the password.
+	 * @param from
+	 * @param to
+	 */
+	protected void mapUser(User from,User to) {
+		to.setUsername(from.getUsername());
+		to.setFirstName(from.getFirstName());
+		to.setLastName(from.getLastName());
+		to.setEmail(from.getEmail());
+		to.setRoles(from.getRoles());
+	}
+
+	@Override
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+	public void deleteUser(Long id) throws Exception {
+		User user = getUserById(id);
+		repository.delete(user);
+	}
+
+	@Override
+	public User changePassword(ChangePasswordForm form) throws Exception {
+		User user = getUserById(form.getId());
+		
+		if (!loggedUserAdmin() && !user.getPassword().equals(form.getCurrentPassword())) {
+			throw new Exception ("Current Password invalido.");
+		}
+		
+		if( user.getPassword().equals(form.getNewPassword())) {
+			throw new Exception ("Nuevo debe ser diferente al password actual.");
+		}
+		
+		if( !form.getNewPassword().equals(form.getConfirmPassword())) {
+			throw new Exception ("Nuevo Password y Current Password no coinciden.");
+		}
+		String encodedPassword = bCryptPasswordEncoder.encode(form.getNewPassword());
+		user.setPassword(encodedPassword);
+		return repository.save(user);
+	}
+
+	public boolean loggedUserAdmin() {
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		UserDetails loggedUser = null;
+		Object roles = null; 
+		if (principal instanceof UserDetails) {
+			loggedUser = (UserDetails) principal;
+		
+			roles = loggedUser.getAuthorities().stream()
+					.filter(x -> "ADMIN".equals(x.getAuthority() ))      
+					.findFirst().orElse(null); //loggedUser = null;
+		}
+		return roles != null ?true :false;
+	}
+}
